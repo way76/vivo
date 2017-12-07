36573761d48fc6dd76fee028683ed4dedf1a7603
Kenshou Uemura <e3mur4@gmail.com>
2017-12-07 14:47:27 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# ![VIVO](logo.png) VIVO Guide
+
+- **Follow the guide carefully and read everything before you ask any question!**
+- **Do not forget step 4. It really means a lot to me. Thanks!**
+
+Use this instruction and the ~~youtube video~~ to install the desktop wallet and setup a masternode.
+This guide is for the creation of separate Controller Wallet & Masternode.
+For Security reasons, THIS IS THE PREFERRED way to run a Masternode. By running your Masternode in this way you are protecting
+your coins in your private wallet, and are not required to have your local wallet running after the Masternode has been started successfully.
+Your coins will be safe if the masternode server is hacked.
+
+## Table of Content
+* [1. Desktop Wallet Preparation](#1-desktop-wallet-preparation-)
+	* [1.1 Setting up desktop wallet](#11-setting-up-desktop-wallet)
+* [2. Masternode Setup](#2-masternode-setup-)
+	* [2.1 Send the coins to your wallet](#21-send-the-coins-to-your-wallet)
+	* [2.2 Create VPS server](#22-create-vps-server)
+	* [2.3 Automatic Masternode Setup](#23-automatic-masternode-setup)
+	* [2.4 Add masternode to the desktop wallet](#24-add-masternode-to-the-desktop-wallet)
+* [3. FAQ](#3-faq)
+* [4. The last and the most important step](#4-support)
+
+## 1. Desktop Wallet Preparation
+
+### 1.1 Setting up desktop wallet
+1. Download and extract the wallet: [win-x64](https://github.com/vivocoin/vivo/releases/download/v0.12.1.7/vivo-0.12.1.7-win64.zip), [win-x32](https://github.com/vivocoin/vivo/releases/download/v0.12.1.7/vivo-0.12.1.7-win32.zip), [linux-x64](https://github.com/vivocoin/vivo/releases/download/v0.12.1.7/vivo-0.12.1.7-ubuntu14.04.tar.xz), [osx](https://github.com/vivocoin/vivo/releases/download/v0.12.1.7/vivo-0.12.1.7-osx.dmg) [wallet-github](https://github.com/vivocoin/vivo/releases)
+1. Start the wallet (vivo-qt) and select the default data directory. After close the wallet. (This creates the folder structure)
+1. Optionally download [blockchain.rar]() file for faster synchronization and extract it to `%appdata%/VivoCore/` folder. Override the necessary files.
+1. Start the wallet again and wait for the synchronization. (10min to 1h)
+1. You can optionaly encrypt the wallet (Settings => Encypt wallet) for security reasons. Do not forget the password or you lose the coins that you have.
+1. Backup `%appdata%/TerracoinCore/wallet.dat` file. This contains your coins. DO NOT LOSE IT!
+	
+## 2. Masternode Setup 
+
+### 2.1 Send the coins to your wallet
+1. Create a new receiving address. (File => Receiving address => New) then copy the address (right click => copy address)
+1. Send exactly 1000 coins to this address. (One transaction, pay attention to the fee)
+1. Wait for the conformation.
+1. Save the transaction id, index `masternode outputs`, and generate and save a new masternode private key `masternode genkey` using the debug console (Tools => Debug Console)
+
+### 2.2 Create VPS server
+1. Register on [vultr](https://www.vultr.com/?ref=7205683). (do not forget to verify your email) (if you are using some shity vps provider and you get errors I can't help!)
+1. Send some money (Billing => Make Payment) to your account to deploy a server. (1 server cost 5$/mo, you can pay with bitcoin on vultr)
+1. Deploy a new server.
+    - Server Type: Ubuntu 16.04
+    - Server Size: 5$/mo, 1GB memory
+
+### 2.3 Automatic Masternode Setup
+1. Download [putty](https://the.earth.li/~sgtatham/putty/latest/w64/putty-64bit-0.70-installer.msi)
+1. Start putty and login as root user. (Root password and server ip address is in vultr overview tab)
+1. Paste this command and answer the questions:
+```
+apt-get install -y python ; rm vivo.py; wget https://raw.githubusercontent.com/u3mur4/vivo/master/vivo.py && python vivo.py
+```
+#### What does the script?
+- updates the system
+- secure the server (setups a firewall)
+- download and install the wallet
+- configure the masternode with rpcusername, rpcpassword and mn's private key (the mn is running under mn1 username)
+- download the boostrap file for faster synchronization
+- setup sentinel if it is available
+- setup some cronjob for automatized jobs (autostart masternode on system start, run sentinel every minutes, etc...)
+
+### 2.4 Add masternode to the desktop wallet
+1. Open `%appdata%/VivoCore/vivo.conf` and add a new line to it. The line format is:<br> 'AliasName ServerIP:ServerPort PrivateKey TransactionID TransactionIndex'
+1. Open the wallet, wait for synchronization, unlock the wallet
+1. Go to Masternodes tab (Setting => Options => Wallet => Show Masternode Tab)
+1. Click Start All
+1. Wait around 2-10 hour to start receiving coins. Check the masternode address for rewards here: [explorer](http://vivo.explorerz.top:3003/) or use your wallet.
+
+## 3. FAQ
+
+1. What if I restart the server?
+	- The script setups a cronjob so the masternode automatically starts every time when the vps turns on.
+1. How to get masternode profit?
+	- Enable coin controll feature (Settings => Options => Wallet => Enable coin controll features)
+	- Go to send tab
+	- Click the inputs button and select only the reward lines.
+	- Click OK
+	- You can now send only the selected amount.
+	- Note: DO NOT EVER Transfer your coins from that original 1k deposit or you'll break your Masternode.
+1. What is the password for the mn1 accounts?
+	- There is no default password. When you create a user it does not have a password yet, so you cannot login with that username until you create a password. There is one other way to act as a new user without its password. As root type `su - mn1`
+	- You need to set a password for the user. Use the passwd command: `passwd mn1`
+1. I get the following error: "Could not allocate vin"
+	- Make sure your wallet fully synced and UNLOCKED.
+	- Make sure the masternode address contains exactly 1000 coins.
+1. How many masternodes can I run using one IP/server?
+	- You can only use one masternode per ip address.
+1. How do I delete the masternode?
+ 	```
+ 	userdel -r -f mn1
+ 	```
+1. I got stuck. Can you help me?
+	- First of all try to get help from the [community](https://t.me/VIVOcoin)
+	- Write me on [telegram](https://t.me/u3mur4) 
+	- Write me an email (do not ask help here): `e3mur4@gmail.com`
+
+# 4. Support
+
+**Send a small amount of coin if you found the guide helpful.**
+
+| Coin      | Symbol | Address                                    |
+| ----------| -------| -------------------------------------------|
+| VIVO | VIVO    | |
+| Ethereum	| ETH    | 0x9a794240b456B8dD5593a7e8d7AE92f4ca4D9D2f |
+| Bitcoin	| BTC    | 33CrDPyMpcwJFyMTceVMTLJYLR8zBSsnWm          |
+
+	
---
+++ vivo.py
@@ -0,0 +1,280 @@
+#! /usr/bin/env python
+from subprocess import Popen,PIPE,STDOUT
+import collections
+import os
+import sys
+import time
+import math
+import os
+import time
+from urllib2 import urlopen
+
+BOOTSTRAP_URL = ""
+SENTINEL_GIT_URL = "https://github.com/vivocoin/sentinel.git"
+
+MN_USERNAME = "mn1"
+MN_PORT = 12845
+MN_RPCPORT = 12800
+MN_NODELIST = "https://pastebin.com/raw/fsd2Acm0"
+
+MN_LFOLDER = ".vivocore"
+MN_WFOLDER = "VivoCore"
+MN_CONFIGFILE = "vivo.conf"
+MN_DAEMON = "vivo-cli"
+MN_CLI = "vivod"
+MN_EXPLORER = "http://vivo.explorerz.top:3003/"
+
+MN_SWAPSIZE = "2G"
+SERVER_IP = urlopen('http://ip.42.pl/raw').read()
+DEFAULT_COLOR = "\x1b[0m"
+PRIVATE_KEY = ""
+
+def print_info(message):
+    BLUE = '\033[94m'
+    print(BLUE + "[*] " + str(message) + DEFAULT_COLOR)
+    time.sleep(1)
+
+def print_warning(message):
+    YELLOW = '\033[93m'
+    print(YELLOW + "[*] " + str(message) + DEFAULT_COLOR)
+    time.sleep(1)
+
+def print_error(message):
+    RED = '\033[91m'
+    print(RED + "[*] " + str(message) + DEFAULT_COLOR)
+    time.sleep(1)
+
+def get_terminal_size():
+    import fcntl, termios, struct
+    h, w, hp, wp = struct.unpack('HHHH',
+        fcntl.ioctl(0, termios.TIOCGWINSZ,
+        struct.pack('HHHH', 0, 0, 0, 0)))
+    return w, h
+    
+def remove_lines(lines):
+    CURSOR_UP_ONE = '\x1b[1A'
+    ERASE_LINE = '\x1b[2K'
+    for l in lines:
+        sys.stdout.write(CURSOR_UP_ONE + '\r' + ERASE_LINE)
+        sys.stdout.flush()
+
+def run_command_as(user, command):
+    run_command('su - {} -c "{}" '.format(user, command))
+
+def run_command(command):
+    out = Popen(command, stderr=STDOUT, stdout=PIPE, shell=True)
+    lines = []
+    
+    while True:
+        line = out.stdout.readline()
+        if (line == ""):
+            break
+        
+        # remove previous lines     
+        remove_lines(lines)
+        
+        w, h = get_terminal_size()
+        lines.append(line.strip().encode('string_escape')[:w-3] + "\n")
+        if(len(lines) >= 9):
+            del lines[0]
+
+        # print lines again
+        for l in lines:
+            sys.stdout.write('\r')
+            sys.stdout.write(l)
+        sys.stdout.flush()
+
+    remove_lines(lines) 
+    out.wait()
+
+def print_welcome():
+    os.system('clear')
+    BLUE = '\033[36m'
+    print(BLUE + "         (              )   " + DEFAULT_COLOR)
+    print(BLUE + "         )\ )        ( /(   " + DEFAULT_COLOR)
+    print(BLUE + " (   (  (()/( (   (  )\())  " + DEFAULT_COLOR)
+    print(BLUE + " )\  )\  /(_)))\  )\((_)\   " + DEFAULT_COLOR)
+    print(BLUE + "((_)((_)(_)) ((_)((_) ((_)  " + DEFAULT_COLOR)
+    print(BLUE + "\ \ / / |_ _|\ \ / / / _ \  " + DEFAULT_COLOR)
+    print(BLUE + " \ V /   | |  \ V / | (_) | " + DEFAULT_COLOR)
+    print(BLUE + "  \_/   |___|  \_/   \___/  " + DEFAULT_COLOR)
+    print(BLUE + "                            " + DEFAULT_COLOR)
+    print_info("VivoCoin masternode installer v1.3")
+
+def update_system():
+    print_info("Updating the system...")
+    run_command("apt-get update")
+    run_command("apt-get upgrade -y")
+    run_command("apt-get dist-upgrade -y")
+
+def check_root():
+    print_info("Check root privileges")
+    user = os.getuid()
+    if user != 0:
+        print_error("This program requires root privileges.  Run as root user.")
+        sys.exit(-1)
+
+def secure_server():
+    print_info("Securing server...")
+    run_command("apt-get --assume-yes install ufw")
+    run_command("ufw allow OpenSSH")
+    run_command("ufw allow {}".format(MN_PORT))
+    run_command("ufw default deny incoming")
+    run_command("ufw default allow outgoing")
+    run_command("ufw --force enable")
+
+def setup_wallet():
+    print_info("Allocating swap...")
+    run_command("fallocate -l {} /swapfile".format(MN_SWAPSIZE))
+    run_command("chmod 600 /swapfile")
+    run_command("mkswap /swapfile")
+    run_command("swapon /swapfile")
+
+    f = open('/etc/fstab','r+b')
+    line = '/swapfile   none    swap    sw    0   0 \n'
+    lines = f.readlines()
+    if (lines[-1] != line):
+        f.write(line)
+        f.close()
+
+    print_info("Installing wallet dependencies...")
+    run_command("apt-get -y install software-properties-common nano libboost-all-dev libzmq3-dev libminiupnpc-dev libssl-dev libevent-dev")
+    run_command("add-apt-repository ppa:bitcoin/bitcoin -y")
+    run_command("apt-get update")
+    run_command("apt-get --assume-yes install git unzip libdb4.8-dev libdb4.8++-dev")
+
+    print_info("Downloading wallet...")
+    run_command("wget https://github.com/vivocoin/vivo/releases/download/v0.12.1.7/vivo-0.12.1.7-ubuntu14.04.tar.xz")
+    run_command("tar -xvf vivo-0.12.1.7-ubuntu14.04.tar.xz")
+
+    run_command("mv vivod /usr/local/bin/{}".format(MN_DAEMON))
+    run_command("mv vivo-cli /usr/local/bin/{}".format(MN_CLI))
+
+def setup_masternode():
+    print_info("Setting up masternode...")
+    run_command("useradd --create-home -G sudo {}".format(MN_USERNAME))
+    
+    print_info("Open your desktop wallet config file (%appdata%/{}/{}) and copy\n    your rpc username and password! If it is not there create one! E.g.:\n\trpcuser=[SomeUserName]\n\trpcpassword=[DifficultAndLongPassword]".format(MN_WFOLDER, MN_CONFIGFILE))
+    rpc_username = raw_input("rpcuser: ")
+    rpc_password = raw_input("rpcpassword: ")
+
+    print_info("Open your wallet console (Tools => Debug Console) and create a new masternode private key: masternode genkey")
+    masternode_priv_key = raw_input("masternodeprivkey: ")
+    global PRIVATE_KEY
+    PRIVATE_KEY = masternode_priv_key
+    
+    config = """rpcuser={}
+rpcpassword={}
+rpcallowip=127.0.0.1
+rpcport={}
+port={}
+server=1
+listen=1
+daemon=1
+logtimestamps=1
+mnconflock=1
+masternode=1
+externalip={}:{}
+masternodeprivkey={}
+{}""".format(rpc_username, rpc_password, MN_RPCPORT, MN_PORT, SERVER_IP, MN_PORT, masternode_priv_key, MN_NODELIST)
+
+    # creates folder structure
+    run_command_as(MN_USERNAME, "mkdir -p /home/{}/{}/".format(MN_USERNAME, MN_LFOLDER))
+    run_command_as(MN_USERNAME, "touch /home/{}/{}/{}".format(MN_USERNAME, MN_LFOLDER, MN_CONFIGFILE))
+    
+    print_info("Saving config file...")
+    with open('/home/{}/{}/{}'.format(MN_USERNAME, MN_LFOLDER, MN_CONFIGFILE), 'w') as f:
+        f.write(config)
+        
+    print_info("Downloading blockchain file...")
+    run_command("apt-get --assume-yes install wget")
+    run_command_as(MN_USERNAME, "cd && wget --continue {}".format(BOOTSTRAP_URL))
+    
+    print_info("Unzipping the file...")
+    run_command("apt-get --assume-yes install unrar")
+    filename = BOOTSTRAP_URL[BOOTSTRAP_URL.rfind('/')+1:]
+    run_command_as(MN_USERNAME, "cd && unrar x -u {} {}".format(filename, MN_LFOLDER))
+       
+    os.system('su - {} -c "{}" '.format(MN_USERNAME, MN_DAEMON + ' -daemon'))
+    print_warning("Masternode started syncing in the background...")
+
+def crontab(job):
+    p = Popen("crontab -l -u {} 2> /dev/null".format(MN_USERNAME), stderr=STDOUT, stdout=PIPE, shell=True)
+    p.wait()
+    lines = p.stdout.readlines()
+    if job not in lines:
+        print_info("Cron job doesn't exist yet, adding it to crontab")
+        lines.append(job)
+        p = Popen('echo "{}" | crontab -u {} -'.format(''.join(lines), MN_USERNAME), stderr=STDOUT, stdout=PIPE, shell=True)
+        p.wait()
+
+
+def autostart_masternode():
+    job = "@reboot /usr/local/bin/{}\n".format(MN_DAEMON)
+    crontab(job)
+    
+
+def setup_sentinel():
+    # no sentinel support
+    if SENTINEL_GIT_URL == "":
+        return
+    
+    print_info("Setting up Sentinel (/home/{}/{}/sentinel)...".format(MN_USERNAME, MN_LFOLDER))
+
+    # install dependencies
+    run_command("apt-get -y install python-virtualenv git virtualenv")
+
+    # download and install sentinel
+    run_command_as(MN_USERNAME, "git clone {} /home/{}/{}/sentinel".format(SENTINEL_GIT_URL, MN_USERNAME, MN_LFOLDER))
+    run_command_as(MN_USERNAME, "cd /home/{}/{}/sentinel && virtualenv ./venv ".format(MN_USERNAME, MN_LFOLDER))
+    run_command_as(MN_USERNAME, "cd /home/{}/{}/sentinel && ./venv/bin/pip install -r requirements.txt".format(MN_USERNAME, MN_LFOLDER))
+
+    # run sentinel every minutes
+    job = "* * * * * cd /home/{}/{}/sentinel && SENTINEL_DEBUG=1 ./venv/bin/python bin/sentinel.py 2>&1 >> sentinel.log".format(MN_USERNAME, MN_LFOLDER)
+    crontab(job)
+
+    # try to update sentinel every day
+    job = "* * 1 * * cd /home/{}/{}/sentinel && git pull https://github.com/terracoin/sentinel.git".format(MN_USERNAME, MN_LFOLDER)
+    crontab(job)
+    
+def end():
+
+    mn_base_data = """
+    Alias: Masternode1
+    IP: {}
+    Private key: {}
+    Transaction ID: [The transaction id of the desposit. 'masternode outputs']
+    Transaction index: [The transaction index of the desposit. 'masternode outputs']
+    --------------------------------------------------
+"""
+
+    mn_data = mn_base_data.format(SERVER_IP + ":" + str(MN_PORT), PRIVATE_KEY)
+
+    imp = R"""Vs lbh sbhaq gur thvqr naq guvf fpevcg hfrshy pbafvqre gb fhccbeg zr.\a    GEP: 1Yl4vDuWeRPsjLpz8Ac4gQHZLbQZuO1Qao\a    RGU: 0k9n794240o456O8qQ5593n7r8q7NR92s4pn4Q9Q2s\a    OGP: 33PeQClZcpjWSlZGprIZGYWLYE8mOFfaJz\a\a"""
+
+    print('')
+    print_info(
+"""Masternodes setup finished!
+    Wait until the masternode is fully synced. To check the progress login the 
+    masternode account (su {}) and run the '{} getinfo' command to get
+    the actual block number. Go to {} website to check 
+    the latest block number or use your wallet. After the syncronization is done 
+    add your masternode to your desktop wallet.
+
+Masternode data:""".format(MN_USERNAME, MN_CLI, MN_EXPLORER) + mn_data)
+
+    print_warning(imp.decode('rot13').decode('unicode-escape'))
+
+def main():
+    print_welcome()
+    check_root()
+    update_system()
+    secure_server()
+    setup_wallet()
+    setup_masternode()
+    autostart_masternode()
+    setup_sentinel()
+    end()
+
+if __name__ == "__main__":
+    main()
