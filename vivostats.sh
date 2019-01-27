a9806ef7561b2376958d0132da2b10c3c31c9599
way76 <43696467+way76@users.noreply.github.com>
2019-01-27 13:18:41 +0100

    Update vivostats.sh

--- vivostats.sh
+++ vivostats.sh
@@ -7,6 +7,10 @@ TB_STONE=$(curl -s4 "http://explorer.stonecoin.rocks/api/getblockcount")
 AN_STONE=$(stone-cli masternode count enabled)
 LB_STONE=$(stone-cli getblockcount)
 
+TB_INN=$(curl -s4 "http://explorer.innovacoin.info/api/getblockcount")
+AN_INN=$(innova-cli masternode count enabled)
+LB_INN=$(innova-cli getblockcount)
+
 #definisco masternode vivo
 MN01_VIVO=b96cc9d0e44cf4b6b7b2f80f8f42446af13a427916b881964c8ed667d9bd4937
 MN02_VIVO=57f88b7a779679d699c60407172faeb1a1d0fc72661236e71b94be1cc012cfbb
@@ -32,6 +36,20 @@ MN08_STONE=54cc0800c10f1dd61875a4f3e9b9552aa97a5bb9b5fc31735ae09d91ba457243
 MN09_STONE=1a48f17dd3d022532e95ddba5b7556832117fcb63f2af562073afb7adc43d01e
 MN10_STONE=91c173890793471b4ef89b834e6161996142b2d857d84cfbe40a88967033f5e8
 MN11_STONE=f7641eabdbed2a8a0f8e540456807b7073409b802d024be69c3d77d0a010aef9
+
+#definisco masternode Innova
+MN01_INN=504a6b8625e8c2b7492e3eb59e3da8fd1ba439105765729b02ebd8b51b73ed8a
+MN02_INN=0e414185cc7883547c5d429e90f58eaf0325f69fa39be2ec489fcfdbb7830b05
+MN03_INN=47aadfca48d5094768663f1b0aaee51b47bc4a9f2089f42518c9e59a75b801cc
+MN04_INN=f9c838e65d9fabc434f6ed496a4481a01f404e00b1b3ea480e0d61988291cb39
+MN05_INN=8b21ae59f5ecc18cbf270951968ed73fb6756376eeaddf2d80ec73013091eebb
+MN06_INN=77ba2f27b86303649641a544de2f3318a32fb73d545bac9a4120422c5339225a
+MN07_INN=353e6be4165292ef8bbb391869c657d199e609c06b3cde9c1738e9c50a4be20b
+MN08_INN=59a8fcf54247b56b169a4eb1c67b0b4bbb3d718ac720f932e9648ad1c8bc8f39
+MN09_INN=f2b769e39cb478facabcdbd5a364d753f18d4cbe8774e96872bbc6d33b1b8080
+MN10_INN=0527c44e1a557b6a6be8791bb9db1eda336abc66e533cfe8dbfbd0e66cd0f93c
+MN11_INN=ee251a9735839ae75fead2aebeed4e967a819876dcccf31b0ea50a8bca63f718
+
 
 
 ###########################################################################
@@ -101,6 +119,39 @@ LS=$(($CU - $LS))
 echo $LS
 }
 
+###########################################################################
+###########################################################################
+Calcola_LP_Inn(){
+LP=$(innova-cli masternodelist lastpaidblock | grep -e $1)
+#rimuove tutto fino al primo spazio
+LP=${LP/* /}
+#rimuove la virgola alla fine
+LP=${LP//[,]/}	
+CONF=$(($TB_INN - $LP))
+DIFF=$(($AN_INN - $CONF))
+echo "$CONF ($DIFF)"
+}
+
+Calcola_Status_Inn(){
+LS=$(innova-cli masternodelist status | grep -e $1)
+#rimuove tutto fino al primo spazio
+LS=${LS/* /}
+#rimuove la virgola alla fine
+LS=${LS//[,]/}	
+#rimuovo virgolette dallo stato
+echo $LS| tr -d '"'
+}
+
+Calcola_LastSeen_Inn(){
+LS=$(inn-cli masternodelist lastseen | grep -e $1)
+#rimuove tutto fino al primo spazio
+LS=${LS/* /}
+#rimuove la virgola alla fine
+LS=${LS//[,]/}	
+CU=$(date +%s)
+LS=$(($CU - $LS))
+echo $LS
+}
 
 ###########################################################################
 ###########################################################################
@@ -120,7 +171,6 @@ echo "MN09  Conferme: $(Calcola_LP_Vivo $MN09_VIVO) - $(Calcola_Status_Vivo $MN0
 #echo "MN11  Conferme: $(Calcola_LP_Vivo $MN11_VIVO) - $(Calcola_Status_Vivo $MN11_VIVO) - $(Calcola_LastSeen_Vivo $MN11_VIVO)"
 echo " "
 echo " "
-echo " "
 echo "Stone status"
 echo "Blocchi : $LB_STONE / $TB_STONE  Masternodes: $AN_STONE"
 echo " "
@@ -135,5 +185,21 @@ echo "MN08  Conferme: $(Calcola_LP_Stone $MN08_STONE) - $(Calcola_Status_Stone $
 echo "MN09  Conferme: $(Calcola_LP_Stone $MN09_STONE) - $(Calcola_Status_Stone $MN09_STONE) - $(Calcola_LastSeen_Stone $MN09_STONE)"
 echo "MN10  Conferme: $(Calcola_LP_Stone $MN10_STONE) - $(Calcola_Status_Stone $MN10_STONE) - $(Calcola_LastSeen_Stone $MN10_STONE)"
 echo "MN11  Conferme: $(Calcola_LP_Stone $MN11_STONE) - $(Calcola_Status_Stone $MN11_STONE) - $(Calcola_LastSeen_Stone $MN11_STONE)"
+echo " "
+echo " "
+echo "Innova status"
+echo "Blocchi : $LB_INN / $TB_INN  Masternodes: $AN_INN"
+echo " "
+echo "MN01  Conferme: $(Calcola_LP_Inn $MN01_INN) - $(Calcola_Status_Inn $MN01_INN) - $(Calcola_LastSeen_Inn $MN01_INN)"
+echo "MN02  Conferme: $(Calcola_LP_Inn $MN02_INN) - $(Calcola_Status_Inn $MN02_INN) - $(Calcola_LastSeen_Inn $MN02_INN)"
+echo "MN03  Conferme: $(Calcola_LP_Inn $MN03_INN) - $(Calcola_Status_Inn $MN03_INN) - $(Calcola_LastSeen_Inn $MN03_INN)"
+echo "MN04  Conferme: $(Calcola_LP_Inn $MN04_INN) - $(Calcola_Status_Inn $MN04_INN) - $(Calcola_LastSeen_Inn $MN04_INN)"
+echo "MN05  Conferme: $(Calcola_LP_Inn $MN05_INN) - $(Calcola_Status_Inn $MN05_INN) - $(Calcola_LastSeen_Inn $MN05_INN)"
+echo "MN06  Conferme: $(Calcola_LP_Inn $MN06_INN) - $(Calcola_Status_Inn $MN06_INN) - $(Calcola_LastSeen_Inn $MN06_INN)"
+echo "MN07  Conferme: $(Calcola_LP_Inn $MN07_INN) - $(Calcola_Status_Inn $MN07_INN) - $(Calcola_LastSeen_Inn $MN07_INN)"
+echo "MN08  Conferme: $(Calcola_LP_Inn $MN08_INN) - $(Calcola_Status_Inn $MN08_INN) - $(Calcola_LastSeen_Inn $MN08_INN)"
+echo "MN09  Conferme: $(Calcola_LP_Inn $MN09_INN) - $(Calcola_Status_Inn $MN09_INN) - $(Calcola_LastSeen_Inn $MN09_INN)"
+echo "MN10  Conferme: $(Calcola_LP_Inn $MN10_INN) - $(Calcola_Status_Inn $MN10_INN) - $(Calcola_LastSeen_Inn $MN10_INN)"
+echo "MN11  Conferme: $(Calcola_LP_Inn $MN11_INN) - $(Calcola_Status_Inn $MN11_INN) - $(Calcola_LastSeen_Inn $MN11_INN)"
 
 
