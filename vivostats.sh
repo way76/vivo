64bd9eed57d4dac0f7c182801bdedf50a0a69ab3
way76 <43696467+way76@users.noreply.github.com>
2019-02-07 08:05:43 +0100

    Update vivostats.sh

--- vivostats.sh
+++ vivostats.sh
@@ -218,7 +218,7 @@ echo $LS
 ###########################################################################
 Calcola_LP_Dogec(){
 
-TN = $AN_DOGEC * 2 - 20
+TN =$($AN_DOGEC * 2 - 20)
 LP=$(dogecash-cli  getmasternodewinners $TN | grep $1 -A 2  -B 2)
 echo "TN :$TN:"
 echo "LP :$LP:"
@@ -295,7 +295,7 @@ echo "$LS1"
 ###########################################################################
 Calcola_LP_Snd()
 {
-TN=$AN_SND * 2 - 20
+TN=$($AN_SND * 2 - 20)
 LP=$(snodecoin-cli getmasternodewinners $TN | grep $1 -A 2 -B 2)
 #ottengo una risposta tipo
 # "nHeight": 144649,
